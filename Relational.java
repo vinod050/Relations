7493b1c00b223d7f57b58fa05167286621f60a60
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 17:04:52 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,12 @@
+public class Relational {
+    public static void main(String[] abcd) {
+        int a=12;
+        int b=54;
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}
